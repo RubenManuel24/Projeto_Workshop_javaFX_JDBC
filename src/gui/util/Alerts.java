f46b0df2161d31ce3449261dcbb767a6b08c98e5
Ruben Manuel <rubenadilsonsapalomanuel@gmail.com>
2022-04-26 16:36:48 +0100

    Deleting .settings folder

--- src/gui/util/Alerts.java
+++ src/gui/util/Alerts.java
@@ -1,12 +1,28 @@
 package gui.util;
 
+import java.util.Optional;
+
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 
 public class Alerts {
 
-	public static void showAlert(String tilte, String tipy, String message, AlertType error) {
+	public static void showAlert(String title, String header, String content, AlertType tipe) {
+		Alert alert = new Alert(tipe);
+		alert.setTitle(title);
+		alert.setHeaderText(header);
+		alert.setContentText(content);
+		alert.show();
         
+	}
+	
+	public static Optional<ButtonType> ShowConfirmaton(String title, String content ){
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle(title);
+		alert.setHeaderText(null);
+		alert.setContentText(content);
+		return alert.showAndWait();
 	}
 
 	
